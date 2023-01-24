0592802b6fe9e5cdba98177ce9790852b9b00249
Serghei Iakovlev <egrep@protonmail.ch>
2023-01-25 00:46:29 +0100

    Simplufy the way to install specmatic

--- src/installer.ts
+++ src/installer.ts
@@ -7,7 +7,7 @@ import os from 'os'
 export const TOOL_NAME = 'specmatic'
 
 const FILE_NAME = `${TOOL_NAME}.jar`
-const ROOT_PATH = `${TOOL_NAME}`
+// const ROOT_PATH = `${TOOL_NAME}`
 
 export interface ISpecmaticVersionInfo {
   downloadUrl: string
@@ -15,73 +15,68 @@ export interface ISpecmaticVersionInfo {
   fileName: string
 }
 
-const getOs = (): string => {
-  switch (os.platform()) {
-    case 'win32':
-      return 'windows'
-    case 'darwin':
-      return 'macosx'
-    default:
-      return 'linux'
-  }
-}
-
 const getFileName = (info: ISpecmaticVersionInfo): string | undefined => {
-  const isWindows = getOs() === 'win32'
+  const isWindows = os.platform() === 'win32'
   const tempDir = process.env.RUNNER_TEMP || '.'
   return isWindows ? path.join(tempDir, info.fileName) : undefined
 }
 
-const getLocalDirname = (info: ISpecmaticVersionInfo): string => {
-  return `${ROOT_PATH}-${info.resolvedVersion}-${getOs()}`
-}
+// const getLocalDirname = (info: ISpecmaticVersionInfo): string => {
+//   return `${ROOT_PATH}-${info.resolvedVersion}}`
+// }
 
 async function installSpecmaticVersion(
   info: ISpecmaticVersionInfo
 ): Promise<string> {
   core.info(`Acquiring ${info.resolvedVersion} from ${info.downloadUrl}`)
+
   const downloadPath = await tc.downloadTool(
     info.downloadUrl,
     getFileName(info)
   )
   core.info(`Successfully download specmatic to ${downloadPath}`)
 
-  const localDir = getLocalDirname(info)
-  const localPath = path.join(localDir, FILE_NAME)
+  // const localDir = getLocalDirname(info)
+  // const localPath = path.join(localDir, FILE_NAME)
 
-  await extractSpecmatic(downloadPath, localPath)
-  core.info(`Successfully extracted specmatic to ${localPath}`)
+  // await extractSpecmatic(downloadPath, localPath)
+  // core.info(`Successfully extracted specmatic to ${localPath}`)
 
-  core.info(`Adding ${localDir} to the cache...`)
+  core.info(`Adding ${downloadPath} to the cache...`)
   const cachedDir = await tc.cacheDir(
-    localDir,
+    downloadPath,
     'specmatic',
     info.resolvedVersion,
     undefined
   )
   core.info(`Successfully cached specmatic to ${cachedDir}`)
 
-  createExecutable(cachedDir)
-  return cachedDir
-}
-
-async function extractSpecmatic(
-  downloadPath: string,
-  localPath: string
-): Promise<void> {
-  const localDir = path.dirname(localPath)
-
-  fs.promises.mkdir(localDir, {recursive: true})
-  fs.promises.rename(downloadPath, localPath)
-}
-
-async function createExecutable(basePath: string): Promise<void> {
-  const jarPath = path.join(basePath, FILE_NAME)
-  const executablePath = path.join(basePath, TOOL_NAME)
+  const jarPath = path.join(cachedDir, FILE_NAME)
+  const executablePath = path.join(cachedDir, TOOL_NAME)
 
   fs.writeFileSync(executablePath, `#!/bin/sh\nexec java -jar ${jarPath} "$@"`)
   fs.chmodSync(executablePath, 0o555)
+
+  return cachedDir
 }
+
+// async function extractSpecmatic(
+//   downloadPath: string,
+//   localPath: string
+// ): Promise<void> {
+//   const localDir = path.dirname(localPath)
+
+//   fs.promises.mkdir(localDir, {recursive: true})
+//   fs.promises.rename(downloadPath, localPath)
+// }
+
+// async function createExecutable(basePath: string): Promise<void> {
+//   const jarPath = path.join(basePath, FILE_NAME)
+//   const executablePath = path.join(basePath, TOOL_NAME)
+
+//   fs.writeFileSync(executablePath, `#!/bin/sh\nexec java -jar ${jarPath} "$@"`)
+//   fs.chmodSync(executablePath, 0o555)
+// }
 
 async function getInfoFromDist(
   versionSpec: string
@@ -110,9 +105,7 @@ export async function getSpecmatic(versionSpec: string): Promise<string> {
     core.info('Install from dist')
     downloadPath = await installSpecmaticVersion(info)
   } catch (err) {
-    throw new Error(
-      `Failed to download Specmatic version ${versionSpec}: ${err}`
-    )
+    throw new Error(`Failed to install specmatic v${versionSpec}: ${err}`)
   }
 
   return downloadPath
