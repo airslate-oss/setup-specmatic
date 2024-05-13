4943b35dc205b5b9a3e741fe0265a98ae9c317ca
Serghei Iakovlev <egrep@protonmail.ch>
2024-05-13 09:49:49 +0200

    Bump semver from 7.6.1 to 7.6.2

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -8783,12 +8783,7 @@ class LRUCache {
   }
 
   delete (key) {
-    if (this.map.has(key)) {
-      this.map.delete(key)
-      return true
-    } else {
-      return false
-    }
+    return this.map.delete(key)
   }
 
   set (key, value) {
